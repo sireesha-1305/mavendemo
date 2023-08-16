f68a89ea367eec745c9f6da335b8a0caec76d43a
sireesha-1305 <sireeshakantipudi1305@gmail.com>
2023-08-16 21:40:56 +0530

    1,2,3,4 has been created

---
+++ src/main/java/com/democompany/new_demo_proj/App.java
@@ -0,0 +1,13 @@
+package com.democompany.new_demo_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
